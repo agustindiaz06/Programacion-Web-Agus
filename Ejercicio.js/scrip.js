84e69e972f63ce818d7f9a1a383813b86aae3a9e
agustindiaz <diazagustiin30@gmail.com>
2024-04-29 16:59:38 -0300

    ejercicios scrip

---
+++ Ejercicio.js/scrip.js
@@ -0,0 +1,24 @@
+// Declarar funciones
+
+// funcion tradicional
+/*
+function myFunction() {
+    return 
+}
+*/
+
+// funcion flecha o arrow function
+/*
+const myFunction = () => {
+
+}*/
+
+let contador = 0;
+
+//dato importante -> siempre los nombres de las funciones siguen la nomenclatura camelCase
+
+function incrementarNumero() {
+  //  let number = document.getElementById("number").textContent;
+  contador = contador + 1;
+  document.getElementById("number").textContent = contador;
+}
